cae671c72a532ced1409ccfff077bfd3a6b26022
0silverback0 <fourmarz@yahoo.com>
2023-04-17 22:45:31 -0400

    fixed requirements

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
-from models import connect_db, db, Trainer, Client, Workout
+from models import connect_db, Trainer, Client, Workout
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Deja1218@localhost/open-source'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 connect_db(app)
-   
 
 @app.route('/')
 def index():
-    return 'hey'
+    return 'Hey You!'
+
